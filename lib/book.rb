e8f265b0ff575652cfa0a5b919762eaded0ce1fb
mrquaxh <joe.t.quach@gmail.com>
2021-09-09 16:30:34 -0400

    commit tree 9f57e6fc1e3eb615dc580717e0376f1928fd7ea6

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +10,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
